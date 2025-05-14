d5684cf2abb8279017b202bd28a44617a656d791
sudipandit <112564048+DAJUSP@users.noreply.github.com>
2025-05-14 22:52:21 +0545

    quiz

--- project_3_Quiz App/script.js
+++ project_3_Quiz App/script.js
@@ -1,47 +1,47 @@
-const questions=[
+const questions = [
     {
         question: "which is largest animal in the world ?",
         answers: [
-            {Text: "shark", correct: false},
-            {Text: "Blue whale", correct: True},
-            {Text: "Elephant", correct: false},
-            {Text: "Zibra", correct: false},
+            { Text: "shark", correct: false },
+            { Text: "Blue whale", correct: True },
+            { Text: "Elephant", correct: false },
+            { Text: "Zibra", correct: false },
         ]
     },
     {
-         question: "which is largest animal in the world ?",
+        question: "which is largest animal in the world ?",
         answers: [
-            {Text: "shark", correct: false},
-            {Text: "Blue whale", correct: True},
-            {Text: "Elephant", correct: false},
-            {Text: "Zibra", correct: false},
+            { Text: "shark", correct: false },
+            { Text: "Blue whale", correct: True },
+            { Text: "Elephant", correct: false },
+            { Text: "Zibra", correct: false },
         ]
     },
-     {
-         question: "which is largest animal in the world ?",
+    {
+        question: "which is largest animal in the world ?",
         answers: [
-            {Text: "shark", correct: false},
-            {Text: "Blue whale", correct: True},
-            {Text: "Elephant", correct: false},
-            {Text: "Zibra", correct: false},
+            { Text: "shark", correct: false },
+            { Text: "Blue whale", correct: True },
+            { Text: "Elephant", correct: false },
+            { Text: "Zibra", correct: false },
         ]
     },
-     {
-         question: "which is largest animal in the world ?",
+    {
+        question: "which is largest animal in the world ?",
         answers: [
-            {Text: "shark", correct: false},
-            {Text: "Blue whale", correct: True},
-            {Text: "Elephant", correct: false},
-            {Text: "Zibra", correct: false},
+            { Text: "shark", correct: false },
+            { Text: "Blue whale", correct: True },
+            { Text: "Elephant", correct: false },
+            { Text: "Zibra", correct: false },
         ]
     },
-     {
-         question: "which is largest animal in the world ?",
+    {
+        question: "which is largest animal in the world ?",
         answers: [
-            {Text: "shark", correct: false},
-            {Text: "Blue whale", correct: True},
-            {Text: "Elephant", correct: false},
-            {Text: "Zibra", correct: false},
+            { Text: "shark", correct: false },
+            { Text: "Blue whale", correct: True },
+            { Text: "Elephant", correct: false },
+            { Text: "Zibra", correct: false },
         ]
     }
 ];
@@ -51,17 +51,34 @@ const answerButton = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
-let score =0;
+let score = 0;
 
-function startQuiz(){
+function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
 }
 
-function showQuestion(){
+function showQuestion() {
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML =questionNo + ". " + currentQuestion.question;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.Text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
 }
+
+function resetState(){
+    nextButton.style.display  = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButton.firstChild);
+    }
+}
+
+startQuiz();
